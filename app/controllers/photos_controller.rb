299fe88b3ce5bff1edc7684df405d6d0c62755b4
virgiliourm <vurmenet@chicagobooth.edu>
2023-02-14 01:45:44 +0000

    delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,27 @@
 class PhotosController < ApplicationController
   def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "/photo_templates/index.html.erb"})
+  end
+
+  def show_photo
+    url_photo_id = params.fetch("photo_page")
+    matching_photos = Photo.where({ :id => url_photo_id})
+    @pic_id = matching_photos.at(0)
+
+    matching_comments = Comment.where({ :photo_id => @pic_id.id})
+    @list_of_comments = matching_comments.order({ :created_at => :desc})
+
+    render({ :template => "/photo_templates/photo_page.html.erb"})
+  end
+
+  def delete_photo
+    url_pic_id = params.fetch("deleted")
+    matching_photos = Photo.where({ :id => url_pic_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+
+    redirect_to("/photos")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:photo_page", { :controller => "photos", :action => "show_photo"})
 
+  get("/delete_photo/:deleted", { :controller => "photos", :action => "delete_photo"})
+
 end
